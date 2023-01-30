69904cdf66d37cbb6d73e12ab0287b03a30d0b7f
Andreas Gut <andreasgut@gmx.ch>
2023-01-30 14:52:34 +0100

    Tests angepasst

--- src/AsciiTest.java
+++ src/AsciiTest.java
@@ -2,17 +2,19 @@ import junit.framework.TestCase;
 
 public class AsciiTest extends TestCase {
 
-    public void testBinaryStringToInt() {
-        int intResult1 = Ascii.binarystringToInt("1000001");
-        int intResult2 = Ascii.binarystringToInt("1111111");
-        assert intResult1 == 65;
-        assert intResult2 == 127;
-    }
-
     public void testIntToBinarystring() {
         String stringResult1 = Ascii.intToBinarystring(65);
         String stringResult2 = Ascii.intToBinarystring(5);
         assert stringResult1.equals("1000001");
         assert stringResult2.equals("0000101");
     }
+
+    public void testBinarystringToInt() {
+        int intResult1 = Ascii.binarystringToInt("1000001");
+        int intResult2 = Ascii.binarystringToInt("1111111");
+        assert intResult1 == 65;
+        assert intResult2 == 127;
+    }
+
+
 }
